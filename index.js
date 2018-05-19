bee90306e82563b267b7280d6872fa89edf24e4d
Mike Walker <jerry.kowal@gmail.com>
2018-05-19 20:31:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 const app = "I don't do much."
+
+kittens = [
+  "Milo",
+  "Otis",
+  "Garfield"]
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+  return kittens
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+  return kittens
+}
+
+function destructivelyRemoveLastKitten(name) {
+  kittens.pop()
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+  return kittens
+}
+
+function appendKitten(name) {
+  var newArray = [...kittens]
+  newArray.push(name)
+  return newArray 
+}
+
+function prependKitten(name) {
+  
+}
